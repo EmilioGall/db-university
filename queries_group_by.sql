7bc3f78c59d886c846c1de8b9ad1eb15e55b773d
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-30 19:32:37 +0200

    feat: added query 1 - queries_group_by

--- queries_group_by.sql
+++ queries_group_by.sql
@@ -1,5 +1,7 @@
 -- 1. Count how many enrollees there were each year.
-
+SELECT YEAR(`students`.`enrolment_date`) AS `year_of_enrollment`, COUNT(*) AS `enrollees_number`
+FROM `students`
+GROUP BY YEAR(`students`.`enrolment_date`);
 
 -- 2. Count the teachers who have the office in the same building.
 
